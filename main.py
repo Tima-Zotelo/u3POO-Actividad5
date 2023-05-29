e01d6e223297451a9360c1645c7bd1934d974bfb
Tima-Zotelo <timacjsntvg@gmail.com>
2023-05-29 18:27:14 -0300

    updates

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from coleccion import Coleccion
 if __name__ == '__main__':
     c = Coleccion()
     c.insertarElemento (3, 3)
-    #c.agregarElemento (5)
-    #c.mostrarElemento(0)
+    c.agregarElemento (5)
+    c.mostrarElemento(0)
